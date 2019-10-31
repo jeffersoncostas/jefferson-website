935a24401d869a4f0f397e41dcd70b16745870c2
jefferson <jefferson.costa@osf-global.com>
2019-10-31 11:45:02 -0300

    CHANGE: deleted previous project

--- src/observables/observables.tsx
+++
@@ -1,9 +0,0 @@
-export interface IObservable {
-    registerObserver(): void;
-    removeObserver(observer: IObserver): void;
-    notifyObserver(param: any): void;
-}
-
-export interface IObserver {
-    update(param: any): void;
-}
